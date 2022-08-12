f59a94f3f6a4e3b047f802f6332638cf61522563
Majid-Al <mi.aliyan@gmail.com>
2022-08-12 18:28:16 +0430

    beforJS

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,15 @@
-let git;
-let a;
-let b;
-let c;
+"use strict";
+let bill = 0,
+  tipPersent = 0,
+  peopleNum = 0;
+
+// document.getElementsByName("Thing")[0].addEventListener('change', doThing);
+//
+//
+//const myElement = document.getElementById("demo");
+// myElement.style.color = "red";
+//
+//
+
+function calclutate() {}
+calclutate();
