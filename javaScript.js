620c6101550f50b0fb42ff1744cf78d9e81e164f
Majid-Al <mi.aliyan@gmail.com>
2022-08-15 15:43:54 +0430

    online

--- javaScript.js
+++ javaScript.js
@@ -1,34 +1,87 @@
 "use strict";
-let bill = 0,
-  tipPersent = 0,
-  peopleNum = 0;
+let bill = "",
+  tipPersent = "",
+  peopleNum = "";
 
-const submit = document.getElementById("reset");
-submit.addEventListener("click", myFunction1);
-function myFunction1() {
+//----------------------- to take tip percent from buttom and show the selected one
+function setPercent(percent) {
+  let some = document.getElementsByClassName("theSelectedButton");
+  for (const theSelectedButton of some) {
+    theSelectedButton.classList.remove("theSelectedButton");
+    theSelectedButton.setAttribute("class", "regulator");
+  }
+  let some2 = document.getElementsByClassName("theSelectedButton2");
+  for (const theSelectedButton2 of some2) {
+    theSelectedButton2.classList.remove("theSelectedButton2");
+    theSelectedButton2.setAttribute("class", "regulator2");
+  }
+  event.target.setAttribute("class", "theSelectedButton");
+  document.getElementById("perInput").value = "";
+  tipPersent = percent;
+  calculateFunc();
+}
+function setPercent2(percent) {
+  let some = document.getElementsByClassName("theSelectedButton");
+  for (const theSelectedButton of some) {
+    theSelectedButton.classList.remove("theSelectedButton");
+    theSelectedButton.setAttribute("class", "regulator");
+  }
+  let some2 = document.getElementsByClassName("theSelectedButton2");
+  for (const theSelectedButton2 of some2) {
+    theSelectedButton2.classList.remove("theSelectedButton2");
+    theSelectedButton2.setAttribute("class", "regulator2");
+  }
+  event.target.setAttribute("class", "theSelectedButton2");
+  document.getElementById("perInput").value = "";
+  tipPersent = percent;
+  calculateFunc();
+}
+//-----------------------------------------------------------------RESET BUTTOM
+function resetAll() {
+  (bill = 0), (tipPersent = 0), (peopleNum = 0);
+  document.getElementById("billInput").value = "";
+  document.getElementById("perInput").value = "";
+  document.getElementById("pNumInput").value = "";
+  document.getElementById("monitor1").innerHTML = " $0.00";
+  document.getElementById("monitor2").innerHTML = " $0.00";
+
+  //---------------------- making the selected button disapear
+  let some = document.getElementsByClassName("theSelectedButton");
+  for (const theSelectedButton of some) {
+    theSelectedButton.classList.remove("theSelectedButton");
+    theSelectedButton.setAttribute("class", "regulator");
+  }
+  let some2 = document.getElementsByClassName("theSelectedButton2");
+  for (const theSelectedButton2 of some2) {
+    theSelectedButton2.classList.remove("theSelectedButton2");
+    theSelectedButton2.setAttribute("class", "regulator2");
+  }
+}
+
+function calculateFunc() {
   bill = document.getElementById("billInput").value;
-  tipPersent = document.getElementById("perInput").value;
   peopleNum = document.getElementById("pNumInput").value;
-  let tipvalue = (bill / 100) * tipPersent;
-  let show1 = tipvalue / peopleNum;
-  let show2 = bill / peopleNum + show1;
+  tipPersent == ""
+    ? (tipPersent = document.getElementById("perInput").value)
+    : "";
+  if (bill != "" && tipPersent != "" && peopleNum != "") {
+    let tipvalue = (bill / 100) * tipPersent;
+    let show1 = tipvalue / peopleNum;
+    let show2 = bill / peopleNum + show1;
 
-  document.getElementById("monitor1").innerHTML = ` $${show1.toFixed(2)}`;
-  document.getElementById("monitor2").innerHTML = ` $${show2.toFixed(2)}`;
-  asdsadsdsa;
+    document.getElementById("monitor1").innerHTML = ` $${show1.toFixed(2)}`;
+    document.getElementById("monitor2").innerHTML = ` $${show2.toFixed(2)}`;
+  }
 }
-function settip() {
-  document.getele;
-}
-//---------------------------------------------------------------------------------  change inner Html to an imput value
+//--------------------------------------------------------------------------  change inner Html to an imput value
 //  let set = document.getElementById("myText").value;
 // document.getElementById("sos").innerHTML = "set";
 
-//---------------------------------------------------------------------------------  set attrebute ( class & ...)
+//--------------------------------------------------------------------------  set attrebute ( class & ...)
 // let set = document.getElementById("myText").value;
 // document.getElementById("sos").setAttribute("class", "democlass");
 
-//---------------------------------------------------------------------------------  set attrebute ( class & ...)
+//-----------------------------------------------------------------------  set attrebute ( class & ...)
 // const element = document.getElementById("myBtn");
 // element.addEventListener("click", myFunction1);
 // function myFunction1() {
